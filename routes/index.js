98bf8686449d640cc8698362f3f210c38e66ebf6
Larry Xu <larry@larryxu.com>
2013-12-25 07:06:40 -0800

    Added academic department listing via mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 
 /*
- * GET home page.
+ * GET home page/list of academic departments.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'telebearsRTC' });
+
+var db = require('../mongo').database_connect('departments');
+
+exports.index = function(req, res) {
+  db.departments.find().sort({name: 1}, function(err, departments) {
+    if(err || !departments) console.log('DB error');
+    else {
+      var data = [];
+      for(var i = 0; i < departments.length; i++) {
+        var abb = departments[i].abbreviation;
+        data.push({ name: departments[i].name, abbreviation: abb });
+      }
+
+      res.render('index', { title: 'telebearsRTC - Berkeley real-time course enrollment data', departments: data });
+    }
+  });
 };
