87783d06e50744798daea9c965e4b467879088ea
Larry Xu <larry@larryxu.com>
2013-12-28 05:14:02 -0800

    Fixed search and modified UI

--- mongo.js
+++ mongo.js
@@ -4,30 +4,10 @@
  */
 
 var mongojs = require('mongojs');
-if(process.env.VCAP_SERVICES) {
-  var env = JSON.parse(process.env.VCAP_SERVICES);
-  var mongo = env['mongodb2-2.4.8'][0]['credentials'];
-}
-else {
-  var mongo = {
-    "hostname": "localhost",
-    "port": 27017,
-    "username": "",
-    "password": "",
-    "name": "",
-    "db": "mydb"
-  }
-}
-var generate_mongo_url = function(obj) {
-  obj.hostname = (obj.hostname || 'localhost');
-  obj.port = (obj.port || 27017);
-  obj.db = (obj.db || 'test');
-  if(obj.username && obj.password)
-    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
-  else
-    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
-}
-var mongourl = generate_mongo_url(mongo);
+if(process.env.MONGOHQ_URL)
+  var mongourl = process.env.MONGOHQ_URL
+else
+  var mongourl = 'mydb';
 
 exports.database_connect = function(collection) {
   return mongojs(mongourl, [collection]);
--- routes/search.js
+++ routes/search.js
@@ -8,9 +8,13 @@ var db = require('../mongo').database_connect('departments');
 // query database with given search parameters
 // render course page if found, else render search page
 exports.index = function(req, res) {
-  var query = req.query.q.trim();
+  var query = req.query.q.trim().toUpperCase();
   var separator = query.lastIndexOf(' ');
-  var department = query.substring(0, separator).toUpperCase();
+  var department = query.substring(0, separator);
+  var course = query.substring(separator+1);
+  var course_num = parseInt(course);
+  if(separator < 0 || isNaN(course_num))
+    department = query;
   var aliases = { 'BIOE': 'BIO ENG',
                   'CHEME': 'CHM ENG',
                   'CEE': 'CIV ENG',
@@ -28,23 +32,42 @@ exports.index = function(req, res) {
                   'STATS': 'STAT' }
   if(department in aliases)
     department = aliases[department];
-  var course = query.substring(separator+1).toUpperCase();
 
-  db.departments.find(
-    {
-      $or: [
-        { name: department },
-        { abbreviation: department }
-      ],
-      'courses.course': course
-    },
-    function(err, data) {
-      if(err || !data) console.log('DB error');
-      else if (data.length == 0) {
-        res.render('search', { title: 'Search results for \''+query+'\'', q: query });
+  if(separator < 0 || isNaN(course_num)) {
+    db.departments.find(
+      {
+        $or: [
+          { name: department },
+          { abbreviation: department }
+        ]
+      },
+      function(err, data) {
+        if(err || !data) console.log('DB error');
+        else if (data.length == 0) {
+          res.render('search', { title: 'Search results for \''+query+'\'', q: query });
+        }
+        else
+          res.redirect('/'+encodeURIComponent(data[0].abbreviation.toLowerCase()));
       }
-      else
-        res.redirect('/'+encodeURIComponent(data[0].abbreviation.toLowerCase())+'/'+course.toLowerCase());
-    }
-  );
+    );
+  }
+  else {
+    db.departments.find(
+      {
+        $or: [
+          { name: department },
+          { abbreviation: department }
+        ],
+        'courses.course': course
+      },
+      function(err, data) {
+        if(err || !data) console.log('DB error');
+        else if (data.length == 0) {
+          res.render('search', { title: 'Search results for \''+query+'\'', q: query });
+        }
+        else
+          res.redirect('/'+encodeURIComponent(data[0].abbreviation.toLowerCase())+'/'+course.toLowerCase());
+      }
+    );
+  }
 };
